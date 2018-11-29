7ff1edbeeae525c91470985279c5d4db38e15b16
Roberto Keane <robert96keane@gmail.com>
2018-11-29 19:02:57 +0700

    new app

---
+++ precache-manifest.364166aa9b47796f8b0cb535c56185aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "726ac4dc3387d841e8b8",
+    "url": "/static/js/main.726ac4dc.chunk.js"
+  },
+  {
+    "revision": "909faafa9196dbeeef05",
+    "url": "/static/js/1.909faafa.chunk.js"
+  },
+  {
+    "revision": "726ac4dc3387d841e8b8",
+    "url": "/static/css/main.bbbf4137.chunk.css"
+  },
+  {
+    "revision": "909faafa9196dbeeef05",
+    "url": "/static/css/1.d5a48a9e.chunk.css"
+  },
+  {
+    "revision": "97c7beacd67b2dac129561796e6d6316",
+    "url": "/index.html"
+  }
+];
