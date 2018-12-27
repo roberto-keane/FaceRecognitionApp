4f75b455dd27aa6d47e1b06aca8eaa521d336046
Roberto Keane <robert96keane@gmail.com>
2018-12-27 19:43:14 +0700

    new changes

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+        <div>
+            <div className='white f3'>
+                {'Shiro, your current rank is...'}
+            </div>
+            <div className='white f1'>
+                {'#5'}
+            </div>
+        </div>
+    )
+}
+
+export default Rank;
